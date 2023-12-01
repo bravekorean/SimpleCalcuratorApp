a1093de03c97a6eacd3e0e789691d9329493bc3d
JunBeom Hwang <wnsqja4646@gmail.com>
2023-12-01 22:36:32 +0900

    Tax, percent Convert

--- app/src/main/java/com/example/mycalc/MainActivity.kt
+++ app/src/main/java/com/example/mycalc/MainActivity.kt
@@ -23,14 +23,14 @@ class MainActivity : AppCompatActivity() {
         gridLayout = findViewById(R.id.gridLayout)
 
 
-        // 모든 숫자 및 연산자 버튼에 대한 클릭 리스너 설정
         val buttonIds = listOf(
             R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4,
             R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9,
             R.id.btnPlus, R.id.btnMinus, R.id.btnMultiply, R.id.btnDivide,
-            R.id.btnEquals, R.id.btnClear
+            R.id.btnEquals, R.id.btnClear, R.id.btnPercent, R.id.btnTax
         )
 
+// 기존의 for 루프에 퍼센트 계산 버튼에 대한 클릭 리스너 설정 추가
         for (buttonId in buttonIds) {
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener { onButtonClick(it) }
@@ -46,9 +46,46 @@ class MainActivity : AppCompatActivity() {
         when (button.id) {
             R.id.btnEquals -> evaluateExpression()
             R.id.btnClear -> clearInput()
+            R.id.btnPercent -> appendPercentage() // 퍼센트 계산 버튼에 대한 처리 추가
+            R.id.btnTax -> applyTax()
             else -> appendToInput(buttonText)
         }
     }
+
+    private fun applyTax() {
+        try {
+            val value = currentInput.toDouble()
+            val taxRate = 0.1 // 10% 세율
+            val taxAmount = value * taxRate
+            val totalAmount = value + taxAmount
+
+            val formattedResult = if (totalAmount % 1 == 0.0) {
+                totalAmount.toInt().toString() // 결과가 정수이면 정수로 변환
+            } else {
+                totalAmount.toString() // 아니면 그대로 유지
+            }
+
+            display.text = formattedResult
+            currentInput = formattedResult
+        } catch (e: Exception) {
+            display.text = "오류"
+            currentInput = ""
+        }
+    }
+
+    // 퍼센트 계산 기능 추가
+    private fun appendPercentage() {
+        try {
+            val value = currentInput.toDouble()
+            val percentResult = value / 100
+            display.text = percentResult.toString()
+            currentInput = percentResult.toString()
+        } catch (e: Exception) {
+            display.text = "오류"
+            currentInput = ""
+        }
+    }
+
 
     private fun appendToInput(value: String) {
         currentInput += value
@@ -99,4 +136,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
 
